ce7fecba851c6b834981a5bbc9ff04c116e33923
MinGyu Kim <mingyukim@si-analytics.ai>
2022-05-06 01:58:13 +0900

    add: eslint, prettier rules

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'prettier'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: ['import', 'prettier'],
+  rules: {
+    camelcase: 'error',
+    'no-console': 'warn',
+    'prefer-const': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', ['parent', 'sibling'], 'internal', 'index'],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc' }
+      }
+    ],
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+  }
+}
