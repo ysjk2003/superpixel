5b4ee842c0089de74510455f17132b4e0212ad99
MinGyu Kim <mingyukim@si-analytics.ai>
2022-08-09 22:17:09 +0900

    build: set typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,33 +2,31 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    node: true
+    node: true,
   },
-  extends: [
-    'eslint:recommended',
-    'prettier'
-  ],
+  extends: ["eslint:recommended", "prettier", "plugin:@typescript-eslint/eslint-recommended"],
+  parser: "@typescript-eslint/parser",
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
   },
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module'
+    sourceType: "module",
   },
-  plugins: ['import', 'prettier'],
+  plugins: ["import", "prettier"],
   rules: {
-    camelcase: 'error',
-    'no-console': 'warn',
-    'prefer-const': 'error',
-    'import/order': [
-      'error',
+    camelcase: "error",
+    "no-console": "warn",
+    "prefer-const": "error",
+    "import/order": [
+      "error",
       {
-        groups: ['builtin', 'external', ['parent', 'sibling'], 'internal', 'index'],
-        'newlines-between': 'always',
-        alphabetize: { order: 'asc' }
-      }
+        groups: ["builtin", "external", ["parent", "sibling"], "internal", "index"],
+        "newlines-between": "always",
+        alphabetize: { order: "asc" },
+      },
     ],
-    'prettier/prettier': ['error', { endOfLine: 'auto' }],
-  }
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
+  },
 }
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,11 @@ module.exports = {
   },
   module: {
     rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
       { test: /\.css$/, use: ["style-loader", "css-loader"] },
       {
         test: /\.(png|jpg|svg|gif)$/,
